256c60ae3c78d5c4783958b26367bd2fb702f9db
Giacomo Bagnoli <gbagnoli@gmail.com>
2009-11-18 12:20:23 +0100

    Better sync in test program

--- src/test.c
+++ src/test.c
@@ -6,41 +6,52 @@
 #include <sys/time.h>
 #include <libgettid.h>
 
-#define TNUM 20
+#define TNUM 10
+pthread_mutex_t end_m = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t start_m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t client_end_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t client_start_cond = PTHREAD_COND_INITIALIZER;
 
 void *thread_body(void *arg)
 {
 	pid_t pid;
 	pid_t tid;
-	pid = getpid();
 	tid = syscall(__NR_gettid);
-	printf("[thread_body] PID: %d, TID: %d\n", pid, tid);
-	sleep(2);
-	printf("[thread_body] PID: %d, TID: %d Exiting.\n", pid, tid);
-	return NULL;
+	int *idx = (int*) arg;
+	printf("[thread_body:%d] Started with TID: %d\n", *idx, tid);
+	// signal main thread, child started
+	// wait the main to signal us we can finish
+	printf("[thread_body:%d] Signal start and then waiting for main to signal us\n", *idx);
+	pthread_cond_signal(&client_start_cond);
+	pthread_mutex_lock(&end_m);
+	pthread_cond_wait(&client_end_cond, &end_m);
+	pthread_mutex_unlock(&end_m);
+	pthread_exit(0);
 }
 
 int main (int argc, char *argv[])
 {
-
 	pthread_t threads[TNUM];
 	int i,res;
 	pid_t tid;
+	int *idx;
 	
 	for (i=0; i<TNUM; i++)
 	{
-		pthread_create(&threads[i], NULL, thread_body, NULL);
-		sleep(1);
+		printf("[main]: Starting thread %d\n", i);
+		idx = malloc(sizeof(int));
+		*idx = i;
+		pthread_create(&threads[i], NULL, thread_body, idx);
+		printf("[main]:\twaiting for thread %d to start\n",i);
+		pthread_mutex_lock(&start_m);
+		pthread_cond_wait(&client_start_cond, &start_m);
+		pthread_mutex_unlock(&start_m);
 		if ( (res = pthread_get_tid(threads[i], &tid)) != 0)
 			fprintf(stderr, "%s\n", gettid_strerror(res));
-		else
-			printf("[main]: last-created tid: %d\n",tid); 
+		else 
+			printf("[main]:\tthread %d tid: %d\n",i, tid); 
+		
 	}
-
-	for (i=0; i<TNUM; i++)
-	{
-		pthread_join(threads[i], NULL);
-		printf("[main]: joined thread %d\n", i);
-	}
-	return 0;
+	printf("[main]: wake up all threads\n");
+	pthread_cond_broadcast(&client_end_cond);
 }
